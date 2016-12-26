8933ef40ec2af47c30bf1345f20c3945770e9229
Murat ÖNER <muhackgames@gmail.com>
2016-12-26 22:01:33 +0300

    Added CarouselView Project.

---
+++ MHG.Sample.CarouselView/MHG.Sample.CarouselView/MHG.Sample.CarouselView/CarouselPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MHG.Sample.CarouselView.Helpers;
+using Xamarin.Forms;
+
+namespace MHG.Sample.CarouselView
+{
+    public partial class CarouselPage : ContentPage
+    {
+        public CarouselPage()
+        {
+            InitializeComponent();
+
+            CarouselZoos.ItemsSource = MonkeyHelper.Monkeys;
+        }
+    }
+}
---
+++ MHG.Sample.CarouselView/MHG.Sample.CarouselView/MHG.Sample.CarouselView/Model/Zoo.cs
@@ -0,0 +1,8 @@
+﻿namespace MHG.Sample.CarouselView.Model
+{
+    public class Zoo
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+    }
+}
